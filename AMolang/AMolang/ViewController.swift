c94c43bce6a0333c64b3c5de76fcc12f556d7ab6
유영하 <dudgk514@naver.com>
2019-09-25 19:45:06 +0900

    메인 > 프로젝트 생성.

---
+++ AMolang/AMolang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AMolang
+//
+//  Created by 유영하 on 25/09/2019.
+//  Copyright © 2019 유영하. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
